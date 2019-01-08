52725edc5b40d152c411e03af937ece3cb14b3db
Lee Wadhams <lee.wadhams@valtech.co.uk>
2019-01-08 14:59:27 +0000

    Upgrade to aspnet core 2.2.

--- src/subscriptions-web/Program.cs
+++ src/subscriptions-web/Program.cs
@@ -49,7 +49,7 @@ namespace Esfa.Recruit.Subscriptions.Web
 
         private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseKestrel(c => c.AddServerHeader = false)
+                .ConfigureKestrel(c => c.AddServerHeader = false)
                 .UseStartup<Startup>()
                 .UseUrls("http://localhost:5035")
                 .UseNLog();
